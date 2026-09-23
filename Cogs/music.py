fc003c4edd4d2b29b76222bbe77a62acdc634ffd
Suranavi <navinarts2026@gmail.com>
2026-09-23 11:09:43 +0530

    Update music.py

--- Cogs/music.py
+++ Cogs/music.py
@@ -4,3 +4,14 @@ from discord.ext import commands
 class Music(commands.Cog):
     def __init__(self, columbina: commands.Bot):
         self.columbina = columbina
+
+#join function
+@commands.command()
+async def join(self, ctx: commands.Context):
+    #ignore bots
+    if ctx.author.bot:
+        return
+
+    #check if user in vc
+    if not ctx.author.voice or not ctx.author.voice.channel:
+        ctx.reply(f"{ctx.author.mention} Please join a vc first!", silent = True)
